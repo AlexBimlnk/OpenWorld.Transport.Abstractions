3bc5ae663d3bbea487c735a791f85b4a9cc81f55
centuriin <centuriin@yandex.ru>
2023-05-07 23:43:03 +0300

    add serialization

---
+++ Transport/Serialization/IDeserializer{TSource, TTarget}.cs
@@ -0,0 +1,28 @@
+﻿namespace OpenWorld.Transport.Abstractions.Serialization;
+
+/// <summary>
+/// Описывает десериализатора сообщений из 
+/// <typeparamref name="TSource"/> в <typeparamref name="TTarget"/>.
+/// </summary>
+/// <typeparam name="TSource">
+/// Тип, подвергающийся десериализации.
+/// </typeparam>
+/// <typeparam name="TTarget">
+/// Тип, являющийся результатом десериализации.
+/// </typeparam>
+public interface IDeserializer<TSource, TTarget>
+{
+    /// <summary>
+    /// Десериализует исходный объект.
+    /// </summary>
+    /// <param name="source">
+    /// Исходный объект, подвергающийся десериализации.
+    /// </param>
+    /// <param name="token">
+    /// Токен отмены операции.
+    /// </param>
+    /// <returns>
+    /// <see cref="ValueTask{TResult}"/>, который завершится, когда десериализация будет проведена.
+    /// </returns>
+    public ValueTask<TTarget> DeserializeAsync(TSource source, CancellationToken token);
+}
---
+++ Transport/Serialization/ISerializer{TSource, TTarget}.cs
@@ -0,0 +1,28 @@
+﻿namespace OpenWorld.Transport.Abstractions.Serialization;
+
+/// <summary>
+/// Описывает сериализатора сообщений из 
+/// <typeparamref name="TSource"/> в <typeparamref name="TTarget"/>.
+/// </summary>
+/// <typeparam name="TSource">
+/// Тип, подвергающийся сериализации.
+/// </typeparam>
+/// <typeparam name="TTarget">
+/// Тип, являющийся результатом сериализации.
+/// </typeparam>
+public interface ISerializer<TSource, TTarget>
+{
+    /// <summary>
+    /// Сериализует исходный объект.
+    /// </summary>
+    /// <param name="source">
+    /// Исходный объект, подвергающийся сериализации.
+    /// </param>
+    /// <param name="token">
+    /// Токен отмены операции.
+    /// </param>
+    /// <returns>
+    /// <see cref="ValueTask{TResult}"/>, который завершится, когда сериализация будет проведена.
+    /// </returns>
+    public ValueTask<TTarget> SerializeAsync(TSource source, CancellationToken token);
+}
